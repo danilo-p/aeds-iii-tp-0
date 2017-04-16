62784b7ef6d132ca8b076d9d1f9013c6699b646b
Danilo Pimentel <danilo.pimentel97@gmail.com>
2017-04-16 18:11:11 -0300

    Added .gitignore. Remove ignored files.

--- docs/html/search/all_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['test_2ec',['test.c',['../test_8c.html',1,'']]],
-  ['test_2eh',['test.h',['../test_8h.html',1,'']]],
-  ['testheader',['testHeader',['../test_8c.html#abe8fb7d95d45b6cb4ce1bcd8f3717913',1,'testHeader():&#160;test.c'],['../test_8h.html#abe8fb7d95d45b6cb4ce1bcd8f3717913',1,'testHeader():&#160;test.h']]]
-];
